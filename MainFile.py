cc6cf1272de7c1ca14fb69a781ac22df4ae02b0f
BuntNuggets <33268568+BuntNuggets@users.noreply.github.com>
2019-01-29 13:36:48 -0600

    Add files via upload

---
+++ MainFile.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication)
+from PyQt5.QtGui import QFont, QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(850, 390, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
